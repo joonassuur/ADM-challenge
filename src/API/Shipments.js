44a8cb6d0239189c46439efbf2d14ae29475fffc
Joonas <joonas@shopz.com>
2020-09-30 19:55:13 +0300

    shipments api

---
+++ src/API/Shipments.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const shipmentsAPI = {
+  async getShipments() {
+    return axios.get(
+      "https://bitbucket.org/hpstore/spacex-cargo-planner/raw/204125d74487b1423bbf0453f4dcb53a2161353b/shipments.json",
+    );
+  },
+};
+
+export default shipmentsAPI;
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
-import { Main, Header, Sidebar } from "../Index";
+import React, { useState, useEffect } from "react";
+import { Main, Header, Sidebar, ShipmentsAPI } from "../Index";
 import useStyles from "./Container.styles";
 
 function Container() {
   const classes = useStyles();
   const [isOpen, setOpen] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const { data } = await ShipmentsAPI.getShipments();
+
+      if (data) console.log(data);
+    })();
+  }, []);
 
   return (
     <div className={classes.container}>
