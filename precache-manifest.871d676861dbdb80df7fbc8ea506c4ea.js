d1f4ba6a5c6ba33737c55413d5c6fe9189aaf1f2
Joonas <joonas@shopz.com>
2020-10-03 18:48:04 +0300

    Updates

---
+++ precache-manifest.871d676861dbdb80df7fbc8ea506c4ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555717b9c9d88d4bf512dc80c189c47c",
+    "url": "/ADM-challenge/index.html"
+  },
+  {
+    "revision": "cf19467142804a8cf463",
+    "url": "/ADM-challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a783f6aa8cd616646479",
+    "url": "/ADM-challenge/static/js/2.c891df10.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/ADM-challenge/static/js/2.c891df10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf19467142804a8cf463",
+    "url": "/ADM-challenge/static/js/main.0b527f4c.chunk.js"
+  },
+  {
+    "revision": "b35377adb6825b746e4a",
+    "url": "/ADM-challenge/static/js/runtime-main.a1d8c7d4.js"
+  }
+]);
